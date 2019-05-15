8a4c9b4e5b948b8297cfa4ba4f4b6ed7233b6426
madhupriyamurali <50649243+madhupriyamurali@users.noreply.github.com>
2019-05-15 20:27:53 +0530

    Add files via upload

---
+++ course-api/src/test/java/TestCoreApp.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCoreApp {
+
+	@Test
+	public void test() {
+		System.out.println("Display Id: 01 Topic: SpringBoot Description: Jenkins and SpringBoot Integration");
+	}
+
+}
