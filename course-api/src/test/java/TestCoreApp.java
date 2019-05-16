3163875da4f79a138d5f8841182053bcec2aa4ba
madhupriyamurali <50649243+madhupriyamurali@users.noreply.github.com>
2019-05-16 21:41:25 +0530

    Add files via upload

--- course-api/src/test/java/TestCoreApp.java
+++ course-api/src/test/java/TestCoreApp.java
@@ -1,9 +1,14 @@
+package com.mindtree.test;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+org.springframework.test.context.ContextConfiguration;
+import com.mindtree.springbootstarter;
 
 @RunWith(SpringRunner.class)
+@ContextConfiguration(classes={CourseApiApp.class})
+
 @SpringBootTest
 public class TestCoreApp {
 
